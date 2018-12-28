4077b7488cf41a769aec563f2d36d0783e043a3f
emacslisp <java.gdb@gmail.com>
2018-12-29 09:00:28 +1100

    add ftp source code

---
+++ Scratch/Assemblies.Ftp/FtpCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemblies.Ftp.FtpCommands
+{
+    class FtpCommandHandler
+    {
+        #region Member Variables
+
+        private string m_sCommand;
+        private FtpConnectionObject m_theConnectionObject = null;
+
+        #endregion
+
+        #region Construction
+
+        protected FtpCommandHandler(string sCommand, FtpConnectionObject connectionObject)
+        {
+            m_sCommand = sCommand;
+            m_theConnectionObject = connectionObject;
+        }
+
+        #endregion
+    }
+}
--- Scratch/CSFtp/Program.cs
+++ Scratch/CSFtp/Program.cs
@@ -10,7 +10,36 @@ namespace CSFtp
     {
         static void Main(string[] args)
         {
+            string sMessage = "USER wudi\r\n";
 
+            sMessage = sMessage.Substring(0, sMessage.IndexOf('\r'));
+
+            string sCommand;
+            string sValue;
+
+            int nSpaceIndex = sMessage.IndexOf(' ');
+
+            if (nSpaceIndex < 0)
+            {
+                sCommand = sMessage.ToUpper();
+                sValue = "";
+            }
+            else
+            {
+                sCommand = sMessage.Substring(0, nSpaceIndex).ToUpper();
+                sValue = sMessage.Substring(sCommand.Length + 1);
+            }
+
+            FtpCommands.FtpCommandHandler handler = m_theCommandHashTable[sCommand] as FtpCommands.FtpCommandHandler;
+
+            if (handler == null)
+            {
+                Console.WriteLine("550 Unknown command\r\n");
+            }
+            else
+            {
+                handler.Process(sValue);
+            }
         }
     }
 }
