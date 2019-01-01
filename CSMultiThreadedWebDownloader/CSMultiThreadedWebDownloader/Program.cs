074507eef194fefb24e156779262a2cab45fe383
emacswin <java.gdb@gmail.com>
2019-01-01 18:13:57 +1100

    add CSMultiThreadedWebDownloader

--- CSMultiThreadedWebDownloader/CSMultiThreadedWebDownloader/Program.cs
+++ CSMultiThreadedWebDownloader/CSMultiThreadedWebDownloader/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSMultiThreadedWebDownloader
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+            DateTime lastNotificationTime;
+
+            WebProxy proxy = null;
+        }
     }
 }
